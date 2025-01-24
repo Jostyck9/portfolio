27a65c50cff129689872cacc0ba7bc74119d17a5
Hugo Berthomé <hugo.berthome@gmail.com>
2025-01-23 22:33:09 -0500

    feat(tailwind): replace all css by tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jostyck9.github.io',
-  base: 'portfolio',
-  integrations: [preact()]
+    site: "https://jostyck9.github.io",
+    base: "portfolio",
+    integrations: [preact()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
